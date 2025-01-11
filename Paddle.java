251c1c81d13f7182ab2976ca06d8baa735010908
robbyj1388 <robbyj@mtu.edu>
2025-01-11 13:52:42 -0500

    Added functional movement to player paddles. left paddle is WASD right paddle is ARROW KEYS

--- Paddle.java
+++ Paddle.java
@@ -2,8 +2,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class Paddle extends Rectangle {
-    private double speed = 5.0;
-    
+    private double speed = 10.0;
 
     public Paddle(double x, double y, double width, double height, Color color) {
         super(width, height);
@@ -12,15 +11,45 @@ public class Paddle extends Rectangle {
         setFill(color);
     }
 
+    /**
+     * Move paddles y coord up by subtracting the speed.
+     */
     public void moveUp() {
-        if (getY() > 0) {
+        if (getBoundsInParent().getMaxY() > 0) {
             setY(getY() - speed);
         }
     }
 
+    /**
+     * Move paddles y coord up by add the speed.
+     * 
+     * @param sceneHeight the height of the scene.
+     */
     public void moveDown(double sceneHeight) {
-        if (getY() + getHeight() < sceneHeight) {
+        if (getBoundsInParent().getMaxY() + getHeight() < sceneHeight) {
             setY(getY() + speed);
+        }
+    }
+
+    /**
+     * Move paddles x coord up by subtracting the speed.
+     * 
+     * @param sceneWidth the width of the scene.
+     */
+    public void moveLeft() {
+        if (getBoundsInParent().getMaxX() - getWidth() > 0) {
+            setX(getX() - speed);
+        }
+    }
+
+    /** 
+     * Mov e paddles x coord up by add the sp eed.
+     * 
+     * @param sceneWidth the width of the scene.
+     */
+    public void moveRight(double sceneWidth){
+        if(getBoundsInParent().getMaxX() < sceneWidth){
+            setX(getX() + speed);
         }
     }
 }
