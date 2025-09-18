833970a6e9b53738fe368213b4cd1459d0d00755
Robby Johnson <johnsonrobby@gmail.com>
2025-09-17 22:55:00 -0400

    Fixed some bugs that occurred from the mvn switch

---
+++ hand_tracking.py
@@ -0,0 +1,24 @@
+import cv2
+import mediapipe as mp
+import socket
+import json
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+cap = cv2.VideoCapture(0)
+
+sock = socket.socket()
+sock.connect(('localhost', 5050))
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    result = hands.process(rgb)
+    if result.multi_hand_landmarks:
+        hand = result.multi_hand_landmarks[0]
+        x = hand.landmark[0].x  # wrist position (0–1)
+        y = hand.landmark[0].y
+        sock.sendall(json.dumps({'x': x, 'y': y}).encode() + b'\n')
+
