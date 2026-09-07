2e62355fb93268078379dade033213a7bed4c0f6
Logan M. Blumka <lmblumka@icloud.com>
2026-09-07 13:52:09 -0400

    Updated ball script
    Same ball script, but with updated comments

--- ball.cs
+++ ball.cs
@@ -1,34 +1,55 @@
-
+/**
+ * This is our script connected to our hittable objects.
+ * Currently, we've only set up the object for the objects that the player needs to hit,
+ * distractors will be added later. 
+ * 
+ * This sript controls the object's speed, velocity, and 
+ * is how we currently detect number of hits from each paddle depending on 
+ * the situation that it's in. This script also controls how much force
+ * is transferred to the ball when it is hit by a paddle. 
+ *
+*/
 using Godot;
 
 public partial class ball : RigidBody2D
-{
-    private float startSpeed = 300.0f;
+{   
+    // Static number for force applied when hit by a paddle.
     private float hitForce = 8.0f;
+    // Static number for maximum speed of the ball can reach.
     private float maxSpeed = 900.0f;
 
+    // References our main game node to help with tracking hits
     private area_2d game;
+
+    // Collision area that detects when the ball is hit by a paddle
+    /* 
+        NOTE: reason for this is because previous collision would either not
+             not move the ball or would not count correctly when hit. 
+    */
     private Area2D hitDetector;
 
 
     public override void _Ready()
     {
-        // Get the Game node from the "game" group
+        // Sets our reference to the game node through our game group
         game = GetTree().GetFirstNodeInGroup("game") as area_2d;
 
-        // Get HitDetector child
+        // References our hit detector of the object
         hitDetector = GetNode<Area2D>("HitDetector");
 
         // Disable gravity
         GravityScale = 0;
 
-        // Prevent ball from rotating
+        /* 
+            Since these objects are still images that don't change when hit,
+            we lock the rotation of the objects so they don't change when hit
+        */
         LockRotation = true;
 
-        // Prevent the rigid body from sleeping
+        // POSSIBLY REMOVEABLE. Helps remove velocity
         CanSleep = false;
 
-        // Disable linear and angular damping
+        // Disables air resistance of the object
         LinearDamp = 0;
         AngularDamp = 0;
 
@@ -44,20 +65,19 @@ public partial class ball : RigidBody2D
 
     private void OnHitDetectorBodyEntered(Node2D body)
     {
-        // Only react to CharacterBody2D paddles
+        // Ensures that the objects only interact with the players paddles
         if (body is CharacterBody2D paddle)
         {
             // Get the device ID stored on the paddle
             Variant paddleDeviceId = paddle.GetMeta("device_id");
 
             // Determine where the paddle is relative to the ball
-            Vector2 difference =
-                GlobalPosition - paddle.GlobalPosition;
+            Vector2 difference = GlobalPosition - paddle.GlobalPosition;
 
-
-            // Determine which side of the paddle the ball hit
+            // Determine which side of the paddle the ball hit 
             if (Mathf.Abs(difference.X) > Mathf.Abs(difference.Y))
-            {
+            {   
+                // Left or Right
                 if (difference.X > 0)
                 {
                     GD.Print(paddle.Name," hit ball from LEFT");
@@ -69,6 +89,7 @@ public partial class ball : RigidBody2D
             }
             else
             {
+                // Top or Bottom
                 if (difference.Y > 0)
                 {
                     GD.Print(paddle.Name," hit ball from TOP");
@@ -93,10 +114,11 @@ public partial class ball : RigidBody2D
                 return;
             }
 
-            // -------------------------
-            // Right Cursor
-            // -------------------------
-
+            /*
+                Determines which paddle hit the ball and which side it was on, incrementing the right variable
+                NOTE: For some reason it only counts correctly for the first cursor that hits on each side
+            */
+            // Right area
             if (paddle.Name == "Right Cursor")
             {
                 game.rhits++;
@@ -117,12 +139,8 @@ public partial class ball : RigidBody2D
                     GD.Print(game.rhitsLeftArea);
                 }
             }
-
-
-            // -------------------------
-            // Left Cursor
-            // -------------------------
-
+            
+            // Left area
             else if (paddle.Name == "Left Cursor")
             {
                 game.lhits++;
